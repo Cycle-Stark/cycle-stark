09b2511c0623a13da5431444afe60e444fcea884
Dalmas Ogembo <dalmasogembo@gmail.com>
2023-11-19 02:16:59 +0300

    update small screen info, add modal alert

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -1,28 +1,40 @@
-import { Box, Card, Container, MantineProvider, ScrollArea, Stack } from "@mantine/core";
+import { Box, Card, Container, MantineProvider, ScrollArea, Stack, Text } from "@mantine/core";
 import { theme } from "./../theme";
 import TopBarNavigation from "../components/navigation/TopBarNavigation";
 import CycleStarkLogo from "../components/others/CycleStarkLogo";
 import { Notifications } from '@mantine/notifications';
-import React from "react";
+import React, { useEffect } from "react";
 import AppProvider, { useAppContext } from "../providers/AppProvider";
-import { ModalsProvider } from '@mantine/modals';
+import { ModalsProvider, modals } from '@mantine/modals';
 
 interface IWrapperCard {
     children: React.ReactNode
 }
 const WrapperCard = ({ children }: IWrapperCard) => {
     const { isSmallScreen } = useAppContext()
+    const infoModal = () => modals.open({
+        title: "Quick information",
+        children: (
+            <>
+                <Text>This is on testnet and best works when using a larger screen ie your pc where you have access to your web wallets like ArgentX and Braavos where you can switch to testnet easily</Text>
+            </>
+        )
+    })
+
+    useEffect(() => {
+        infoModal()
+    }, [])
     return (
         <Card p={0} style={{
             borderRadius: "0 0 20px 20px",
-            height: isSmallScreen ? "auto" : "600px",
+            height: isSmallScreen ? "fit-content" : "600px",
             overflowX: "hidden"
         }}>
             {
                 isSmallScreen ? (<Box p={isSmallScreen ? "xs" : "sm"}>
                     {children}
                 </Box>) : (
-                    <ScrollArea className="h-100">
+                    <ScrollArea className="h-100" style={{overflowX: "hidden"}}>
                         <Box p="sm">
                             {children}
                         </Box>
@@ -32,7 +44,7 @@ const WrapperCard = ({ children }: IWrapperCard) => {
         </Card>
     )
 }
- 
+
 
 const MainLayout = (props: any) => {
     const { children } = props
@@ -42,7 +54,7 @@ const MainLayout = (props: any) => {
             <ModalsProvider>
                 <AppProvider>
                     <Notifications />
-                    <Container size={'xl'} px={isSmallScreen ? "xs" : "01"} py={70} className="behind-screen">
+                    <Container size={'xl'} px={isSmallScreen ? "xs" : "0"} py={70} className="behind-screen">
                         <div style={{
                             boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
                             // transform: "perspective(500px) skewY(15deg, -35deg)"
