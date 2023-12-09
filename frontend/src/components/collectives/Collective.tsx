3f44034141d77691f67bb6526783ae89d05615d8
Dalmas Ogembo <dalmasogembo@gmail.com>
2023-12-10 01:14:29 +0300

    Update collective card include USD value

--- frontend/src/components/collectives/Collective.tsx
+++ frontend/src/components/collectives/Collective.tsx
@@ -1,7 +1,12 @@
 import { Avatar, Box, Grid, Stack, Text, Title, useMantineColorScheme } from "@mantine/core"
-import { convertToReadableTokens, bigintToShortStr, isDarkMode } from "../../configs/utils"
+import { convertToReadableTokens, bigintToShortStr, isDarkMode, getRealPrice, bigintToLongStrAddress, formatNumberInternational } from "../../configs/utils"
 import { useHover } from "@mantine/hooks";
 import { NavLink } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useAppContext } from "../../providers/AppProvider";
+import { CairoCustomEnum } from "starknet";
+import TOKENS, { Token } from "../../configs/tokens";
+import BigNumber from "bignumber.js";
 
 interface ICollective {
     collective: any
@@ -10,6 +15,30 @@ interface ICollective {
 const Collective = ({ collective }: ICollective) => {
     const { colorScheme } = useMantineColorScheme()
     const { hovered, ref } = useHover();
+    const [tokenPrice, setTokenPrice] = useState<null | any>()
+    const [asset, setAsset] = useState<null | Token>()
+    const { pragma_contract } = useAppContext()
+
+    const getTokenPrice = async () => {
+        if (pragma_contract && collective) {
+            const token = bigintToLongStrAddress(collective?.token)
+            const asset: any = TOKENS.find(asset => asset.address.toLowerCase() === token.toLowerCase())
+            if (asset) {
+                const SPOTENTRY_ENUM = new CairoCustomEnum({
+                    SpotEntry: asset.pair_id
+                })
+                const res = await pragma_contract.get_data_median(SPOTENTRY_ENUM)
+                const price = getRealPrice(res)
+                setTokenPrice(price?.price)
+                setAsset(asset)
+            }
+        }
+    }
+
+    useEffect(() => {
+        getTokenPrice()
+    }, [])
+
     return (
         <>
             <Box ref={ref} p={4} style={theme => ({
@@ -36,7 +65,7 @@ const Collective = ({ collective }: ICollective) => {
                     <Grid>
                         <Grid.Col span={4} px={'xs'}>
                             <Stack className="h-100" align="center" justify="center">
-                                <Avatar radius={'xl'} size={'lg'} tt={'capitalize'} mx={'auto'}>
+                                <Avatar radius={'xl'} size={'lg'} tt={'capitalize'} mx={'auto'} src={asset?.icon}>
                                     {bigintToShortStr(collective?.name).substring(0, 2)}
                                 </Avatar>
                             </Stack>
@@ -44,7 +73,7 @@ const Collective = ({ collective }: ICollective) => {
                         <Grid.Col span={8}>
                             <Stack gap={4}>
                                 <Title order={3} size={'md'} style={{ fontWeight: '800' }}>{bigintToShortStr(collective?.name)}</Title>
-                                <Text size={'xs'} style={{ fontWeight: '400' }}>{convertToReadableTokens(collective?.cycle_amount, 18)} {bigintToShortStr(collective?.symbol)}/cycle | {collective?.hero_count?.toString() ?? 0} Heroes</Text>
+                                <Text size={'xs'} style={{ fontWeight: '400' }}>{convertToReadableTokens(collective?.cycle_amount, BigNumber(collective?.decimals).toNumber())} {bigintToShortStr(collective?.symbol)} (${formatNumberInternational(BigNumber(convertToReadableTokens(collective?.cycle_amount, BigNumber(collective?.decimals).toNumber())).multipliedBy(tokenPrice).toNumber())}) /cycle | {collective?.hero_count?.toString() ?? 0} Heroes</Text>
                                 <Text size={'sm'} style={{ fontWeight: '600' }}>{bigintToShortStr(collective?.aim)}</Text>
                             </Stack>
                         </Grid.Col>
